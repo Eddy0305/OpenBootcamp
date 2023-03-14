269f1b5eebcd433be079cee074c8181c12271155
Eduardo <55516976+Eddy0305@users.noreply.github.com>
2023-03-14 15:30:34 -0600

    Ejercicio 1 y 2
    Se añadieron los ejercicios 1 y 2 de C#

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Out.WriteLine("Ingresa tu nombre: ");
+string nombre = Console.ReadLine();
+
+Console.Out.WriteLine("Tu nombre es: "+ nombre);
+
+Console.Out.WriteLine(DateTime.Now);
---
+++ Program2.cs
@@ -0,0 +1,1 @@
+Console.Out.WriteLine(DateTime.Now);
