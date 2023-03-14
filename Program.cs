871d65c49f9f8c193d2633667bc197ca97fa570b
Eduardo <55516976+Eddy0305@users.noreply.github.com>
2023-03-14 17:53:05 -0600

    Ejercicio 2 C#
    Se añadió el segundo ejercicio realizado en el curso de C#

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Out.WriteLine("Ingresa tu nombre: ");
 string nombre = Console.ReadLine();
+Console.Out.WriteLine("Ingresa tu apellido: ");
+string apellido = Console.ReadLine();
+Console.Out.WriteLine("Ingresa tu edad en anios: ");
+int edad = Console.Read();
+Console.Out.WriteLine("¿Sabe programar?");
+Console.ReadLine();
+string saber = Console.ReadLine();
+Console.Out.WriteLine("Espera");
+
+
+
 
 Console.Out.WriteLine("Tu nombre es: "+ nombre);
+Console.Out.WriteLine("Tu apellido es: "+ apellido);
+Console.Out.WriteLine("Tu edad: "+ edad);
+Console.Out.WriteLine("Sabes programar: "+ saber);
 
 Console.Out.WriteLine(DateTime.Now);
